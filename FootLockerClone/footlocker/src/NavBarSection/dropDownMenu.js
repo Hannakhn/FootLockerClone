1dbd20bb2f3287375ce2477d6076cd91c3b9cc7b
Hanna Khan <hannakhn@hotmail.com>
2023-09-21 01:18:46 -0400

    Navbar update

--- FootLockerClone/footlocker/src/NavBarSection/dropDownMenu.js
+++ FootLockerClone/footlocker/src/NavBarSection/dropDownMenu.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Collapse,
+  Button,
   Navbar,
   NavbarToggler,
   NavbarBrand,
@@ -11,18 +12,28 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Dropdown,
   NavbarText,
 } from "reactstrap";
 
 function DropdownPortion() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
   return (
     <div>
-      <Nav fixed="top" expand="md" style={{ Color: "white" }} className="inactive">
-        <NavItem >
-          <NavLink active href="#">
+      <Nav expand="md" className="dropDownMenu">
+        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle nav caret>
             Find a Store
-          </NavLink>
-        </NavItem>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>
+              <Button>Find a Store</Button>
+            </DropdownItem>
+            <DropdownItem>Set Store Location</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <NavItem>
           <NavLink href="#">Welcome, Sign In</NavLink>
         </NavItem>
--- FootLockerClone/footlocker/src/NavBarSection/topNavBar.js
+++ FootLockerClone/footlocker/src/NavBarSection/topNavBar.js
@@ -9,27 +9,30 @@ import {
   NavbarBrand,
   Navbar,
 } from "reactstrap";
+import Divider from "@mui/material/Divider";
 import SearchBar from "./SearchBar";
 import DropdownPortion from "./dropDownMenu";
 
 function NavbarTop() {
   return (
     <div>
-      <Navbar className="my-2" color="dark" dark expand="md">
-        <NavbarBrand href="/">
-          <img
-            className="footlockerImageLogo"
-            alt="logo"
-            src="img/logo.jpg"
-            style={{
-              height: 80,
-              width: 230,
-            }}
-          />
-          <SearchBar />
-        </NavbarBrand>
-        <DropdownPortion />
-      </Navbar>
+      <DropdownPortion />
+        <Navbar className="my-2" expand="md">
+      <Divider light>
+          <NavbarBrand href="/">
+            <img
+              className="footlockerImageLogo"
+              alt="logo"
+              src="img/logo.jpg"
+              style={{
+                height: 80,
+                width: 230,
+              }}
+            />
+            <SearchBar />
+          </NavbarBrand>
+      </Divider>
+        </Navbar>
     </div>
   );
 }
